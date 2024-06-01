e4d2306522300dc15832fea5ebb9edbf474c5bf0
Furkan Ünsalan <furkanunsalan@gmail.com>
2024-06-01 12:50:35 +0300

    React Native - Day 5

--- second-project/components/PrimaryButton.js
+++ second-project/components/PrimaryButton.js
@@ -1,18 +1,16 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton(props) {
-    function pressHandler() {
-        console.log('Pressed!')
-    }
+import Colors from "../constants/colors";
 
+function PrimaryButton(props) {
     return (
         <View style={styles.buttonOuterContainer}>
             <Pressable style={({pressed}) =>
                 pressed
                     ? [styles.pressed, styles.buttonInnerContainer]
                     : styles.buttonInnerContainer}
-                       onPress={pressHandler}
-                       android_ripple={{color: '#640233'}}>
+                       onPress={props.onPress}
+                       android_ripple={{color: Colors.primary600}}>
                 <Text style={styles.buttonText}>{props.children}</Text>
             </Pressable>
         </View>
@@ -28,7 +26,7 @@ const styles = StyleSheet.create({
         overflow: "hidden",
     },
     buttonInnerContainer: {
-        backgroundColor: '#72063C',
+        backgroundColor: Colors.primary500,
         paddingVertical: 8,
         paddingHorizontal: 16,
         elevation: 2
--- second-project/screens/StartGameScreen.js
+++ second-project/screens/StartGameScreen.js
@@ -1,19 +1,49 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import {useState} from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+import Colors from "../constants/colors";
+
+function StartGameScreen({ onPickNumber }) {
+    const [enteredNumber, setEnteredNumber] = useState("")
+
+    function numberInputHandler(enteredText) {
+        setEnteredNumber(enteredText);
+    }
+
+    function resetInputHandler() {
+        setEnteredNumber("")
+    }
+
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber)
+
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert(
+                'Invalid Number!',
+                'Number has to be a number between 1 and 99.',
+                [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}]
+                )
+            return;
+        }
+
+        onPickNumber(chosenNumber);
+    }
+
     return (
         <View style={styles.inputContainer}>
             <TextInput style={styles.numberInput}
                        maxLength={2}
                        keyboardType="number-pad"
-                       autoCapitalize="none" autoCorrect={false}/>
+                       autoCapitalize="none" autoCorrect={false}
+                       onChangeText={numberInputHandler}
+                       value={enteredNumber}/>
             <View style={styles.buttonsContainer}>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Reset</PrimaryButton>
+                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
                 </View>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Confirm</PrimaryButton>
+                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
                 </View>
             </View>
         </View>
@@ -29,7 +59,7 @@ const styles = StyleSheet.create({
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
-        backgroundColor: '#4e0329',
+        backgroundColor: Colors.primary800,
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black',
@@ -41,9 +71,9 @@ const styles = StyleSheet.create({
         height: 50,
         width: 50,
         fontSize: 32,
-        borderBottomColor: '#ddb52f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
-        color: '#ddb52f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: "bold",
         textAlign: "center"
