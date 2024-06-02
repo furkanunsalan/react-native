dd6627e6b4460486e34162c44145f5c4b0f3f8d4
Furkan Ünsalan <furkanunsalan@gmail.com>
2024-06-02 13:01:57 +0300

    React Native - Day 6

--- second-project/screens/GameScreen.js
+++ second-project/screens/GameScreen.js
@@ -1,15 +1,78 @@
-import {Text, View, StyleSheet} from "react-native";
+import { View, StyleSheet, Alert} from "react-native";
+import {useState, useEffect} from "react";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function GameScreen({userNumber, onGameOver}) {
+    const initialGuess = generateRandomBetween(1, 100, userNumber)
+    const [currentGuess, setCurrentGuess] = useState(initialGuess)
+
+    useEffect(() => {
+        if (currentGuess === userNumber) {
+            onGameOver()
+        }
+    }, [currentGuess, userNumber, onGameOver])
+
+    function nextGuessHandler(direction) { // direction => 'lower', 'greater'
+        if (
+            (direction === 'lower' && currentGuess < userNumber) ||
+            (direction === 'greater' && currentGuess > userNumber)
+        ) {
+            Alert.alert("Don't lie!", "You know that his is wrong...", [
+                {text: 'Sorry!', style: "cancel"}
+            ])
+            return
+        }
+
+        if (direction === 'lower') {
+            maxBoundary = currentGuess;
+        } else {
+            minBoundary = currentGuess + 1
+        }
+
+        const newRndNumber = generateRandomBetween(
+            minBoundary,
+            maxBoundary,
+            currentGuess);
+        setCurrentGuess(newRndNumber)
+    }
+
     return (
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
-            <View>
-                <Text>Higher or lower ?</Text>
-            </View>
-            <View></View>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card>
+                <InstructionText style={styles.instructionText}>Higher or lower ?</InstructionText>
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')} >
+                            <AntDesign name="down" size={24} color="white" />                        </PrimaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                            <AntDesign name="up" size={24} color="white" />
+                        </PrimaryButton>
+                    </View>
+                </View>
+            </Card>
         </View>
     )
 }
@@ -21,5 +84,13 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 24
     },
-
+    instructionText: {
+        marginBottom: 12,
+    },
+    buttonsContainer: {
+        flexDirection: "row",
+    },
+    buttonContainer: {
+        flex: 1,
+    }
 })
